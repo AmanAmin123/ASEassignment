95bd491c4896508aff023cde4b37be51d783fcb8
AmanAmin123 <125648691+AmanAmin123@users.noreply.github.com>
2023-04-05 02:48:36 +0100

    Create CommandParser.cs
    commands are given which shows what the user has to type for the form designer to work such as drawTo and moveTo. lines 14,15,16 allows the position of the pen to be remembered so it doesn't get reset every time a new position is ran.

---
+++ CommandParser.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShapesApp
+{
+    public class CommandParser
+    {
+        // stores graphics and pen position information
+        private Graphics graphics;
+        private Point currentPosition;
+        private Point initialPosition;
+
+        // initializes graphics and pen position
+        public CommandParser(Graphics graphics, Point initialPosition)
+        {
+            this.graphics = graphics;
+            this.initialPosition = initialPosition;
+            Reset();
+        }
+
+        public void Execute(string command)
+        {
+            try
+            {
+                var parts = command.Split(' ');
+                var action = parts[0].ToLower();
+
+                // switch statement to handle different command types
+                switch (action)
+                {
+                    // move pen to a given position
+                    case "moveto":
+                        MoveTo(int.Parse(parts[1]), int.Parse(parts[2]));
+                        break;
+                    // draw a line from current position to a given position
+                    case "drawto":
+                        DrawTo(int.Parse(parts[1]), int.Parse(parts[2]));
+                        break;
+                    // clear the drawing area and reset pen position
+                    case "clear":
+                        Clear();
+                        break;
+                    // reset pen position to initial position
+                    case "reset":
+                        Reset();
+                        break;
+                    // draw a rectangle at current position with given dimensions
+                    case "rectangle":
+                        DrawRectangle(int.Parse(parts[1]), int.Parse(parts[2]));
+                        break;
+                    // draw a circle at current position with given radius
+                    case "circle":
+
+                        DrawCircle(int.Parse(parts[1]));
+                        break;
+                }
+            }
+            // catch block to handle invalid command format
+            catch (Exception)
+            {
+                MessageBox.Show("Not a valid command");
+
+            }
+        }
